d379332045ba4ce5f340836721ba1e5ee7c2e484
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2021-03-22 22:24:48 +0100

    Add twitch chat messages to minecraft chat

--- src/main/java/me/mini_bomba/streamchatmod/StreamChatMod.java
+++ src/main/java/me/mini_bomba/streamchatmod/StreamChatMod.java
@@ -1,21 +1,80 @@
 package me.mini_bomba.streamchatmod;
 
-import net.minecraft.init.Blocks;
+import com.github.philippheuer.credentialmanager.domain.OAuth2Credential;
+import com.github.twitch4j.TwitchClient;
+import com.github.twitch4j.TwitchClientBuilder;
+import com.github.twitch4j.chat.TwitchChat;
+import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.IChatComponent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLModDisabledEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = StreamChatMod.MODID, version = StreamChatMod.VERSION, clientSideOnly = true, name = StreamChatMod.MODNAME)
+@Mod(modid = StreamChatMod.MODID, version = StreamChatMod.VERSION, clientSideOnly = true, useMetadata = true)
 public class StreamChatMod
 {
     public static final String MODID = "streamchatmod";
     public static final String MODNAME = "StreamChat";
     public static final String VERSION = "1.0";
+    private StreamConfig config;
+    private TwitchClient twitch = null;
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void postInit(FMLPostInitializationEvent event)
     {
-		// some example code
-        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+		startTwitch();
+    }
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        config = new StreamConfig(event.getSuggestedConfigurationFile());
+    }
+
+    @EventHandler
+    public void stop(FMLModDisabledEvent event) {
+        stopTwitch();
+    }
+
+    public void startTwitch() {
+        if (!config.twitchEnabled.getBoolean() || config.twitchChannels.getStringList().length == 0) return;
+        String token = config.twitchToken.getString();
+        OAuth2Credential credential = new OAuth2Credential("twitch", token);
+        twitch = TwitchClientBuilder.builder()
+                .withDefaultAuthToken(credential)
+                .withEnableChat(true)
+                .withChatAccount(credential)
+                .build();
+        twitch.getEventManager().onEvent(ChannelMessageEvent.class, this::onTwitchMessage);
+        TwitchChat chat = twitch.getChat();
+        chat.connect();
+        for (String channel : config.twitchChannels.getStringList()) {
+            chat.joinChannel(channel);
+        }
+    }
+
+    private void onTwitchMessage(ChannelMessageEvent event) {
+        sendLocalMessage(new ChatComponentText(EnumChatFormatting.DARK_PURPLE+"[TWITCH"+(config.showChannelName.getBoolean() ? "/"+event.getChannel().getName() : "")+"]"+EnumChatFormatting.WHITE+" <"+event.getUser().getName()+"> "+event.getMessage()));
+    }
+
+    private void sendLocalMessage(IChatComponent chat) {
+        EntityPlayerSP player = Minecraft.getMinecraft().thePlayer;
+        if (player == null) return;
+        Minecraft.getMinecraft().thePlayer.addChatMessage(chat);
+    }
+
+    public void stopTwitch() {
+        if (twitch == null) return;
+        TwitchChat chat = twitch.getChat();
+        for (String channel : chat.getChannels()) {
+            chat.leaveChannel(channel);
+        }
+        chat.disconnect();
+        twitch.close();
     }
 }
---
+++ src/main/java/me/mini_bomba/streamchatmod/StreamConfig.java
@@ -0,0 +1,25 @@
+package me.mini_bomba.streamchatmod;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.common.config.Property;
+
+import java.io.File;
+
+public class StreamConfig {
+    private final Configuration config;
+    protected final Property showChannelName;
+    protected final Property twitchToken;
+    protected final Property twitchEnabled;
+    protected final Property twitchChannels;
+    protected final Property twitchDefaultChannel;
+
+    public StreamConfig(File configFile) {
+        config = new Configuration(configFile);
+        showChannelName = config.get("common", "showChannelName", false);
+        twitchToken = config.get("tokens", "twitch", "");
+        twitchEnabled = config.get("twitch", "enabled", false);
+        twitchChannels = config.get("twitch", "channels", new String[0]);
+        twitchDefaultChannel = config.get("twitch", "default_channel", "");
+        if (config.hasChanged()) config.save();
+    }
+}
