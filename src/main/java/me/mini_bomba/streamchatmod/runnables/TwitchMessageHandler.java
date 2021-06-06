5587a57fe64071970888c7fc942e3b716e4caf1e
mini-bomba <55105495+mini-bomba@users.noreply.github.com>
2021-06-06 21:30:46 +0200

    Add user prefixes to messages, change appearance of Twitch messages

--- src/main/java/me/mini_bomba/streamchatmod/runnables/TwitchMessageHandler.java
+++ src/main/java/me/mini_bomba/streamchatmod/runnables/TwitchMessageHandler.java
@@ -1,6 +1,7 @@
 package me.mini_bomba.streamchatmod.runnables;
 
 import com.github.twitch4j.chat.events.channel.ChannelMessageEvent;
+import com.github.twitch4j.common.enums.CommandPermission;
 import me.mini_bomba.streamchatmod.StreamChatMod;
 import me.mini_bomba.streamchatmod.StreamUtils;
 import net.minecraft.event.ClickEvent;
@@ -8,6 +9,8 @@ import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatStyle;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.IChatComponent;
+
+import java.util.Set;
 
 public class TwitchMessageHandler implements Runnable {
     private final ChannelMessageEvent event;
@@ -21,7 +24,13 @@ public class TwitchMessageHandler implements Runnable {
     @Override
     public void run() {
         boolean showChannel = mod.config.forceShowChannelName.getBoolean() ||(mod.twitch != null && mod.twitch.getChat().getChannels().size() > 1);
-        IChatComponent component = new ChatComponentText(EnumChatFormatting.DARK_PURPLE+"[TWITCH"+(showChannel ? "/"+event.getChannel().getName() : "")+"]"+EnumChatFormatting.WHITE+" <"+event.getUser().getName()+"> "+event.getMessage());
+        Set<CommandPermission> perms = event.getPermissions();
+        String prefix = perms.contains(CommandPermission.BROADCASTER) ? EnumChatFormatting.RED + " STREAMER " :
+                      ( perms.contains(CommandPermission.TWITCHSTAFF) ? EnumChatFormatting.BLACK + " STAFF " :
+                      ( perms.contains(CommandPermission.MODERATOR) ? EnumChatFormatting.GREEN + " MOD " :
+                      ( perms.contains(CommandPermission.VIP) ? EnumChatFormatting.DARK_PURPLE + " VIP " :
+                      ( perms.contains(CommandPermission.SUBSCRIBER) ? EnumChatFormatting.GOLD + " SUB " : " "))));
+        IChatComponent component = new ChatComponentText(EnumChatFormatting.DARK_PURPLE+"[TWITCH"+(showChannel ? "/"+event.getChannel().getName() : "")+"]"+ prefix + EnumChatFormatting.WHITE + event.getUser().getName() + EnumChatFormatting.GRAY+" >> "+EnumChatFormatting.WHITE+event.getMessage());
         ChatStyle style = new ChatStyle().setChatClickEvent(new ClickEvent(ClickEvent.Action.SUGGEST_COMMAND, "/twitch delete " + event.getChannel().getName() + " " + event.getMessageEvent().getMessageId().orElse("")));
         component.setChatStyle(style);
         StreamUtils.addMessage(component);
